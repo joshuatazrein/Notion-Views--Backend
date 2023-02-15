cd67e8b31f5918fd4caec16a930bd28f16516ebf
Joshua Tazman Reinier <jreinier@oberlin.edu>
2023-02-15 14:54:51 -0500

    packaged for serving

---
+++ dist/background.js
@@ -0,0 +1,26 @@
+import { processRequest } from './backgroundApi';
+chrome.runtime.onMessage.addListener(({ type, action, data, access_token }, sender, sendResponse) => {
+    if (type === 'auth') {
+        switch (action) {
+            case 'google.getToken':
+                chrome.identity.getAuthToken({}, token => {
+                    console.log('token is', token);
+                    sendResponse(token);
+                });
+                break;
+        }
+    }
+    else {
+        if (access_token === 'GOOGLE_TOKEN') {
+            // pass in a dummy for the Chrome extension
+            chrome.identity.getAuthToken({}, access_token => {
+                console.log('got access token:', access_token, 'for request:', action);
+                processRequest(type, action, data, sendResponse, access_token);
+            });
+        }
+        else {
+            processRequest(type, action, data, sendResponse, access_token);
+        }
+    }
+    return true;
+});
